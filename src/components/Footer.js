bd59f243c4c94f4c4931786578c265eabe9938b1
QED0711 <qdizon@gmail.com>
2019-09-15 14:59:23 -0400

    footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className='footer-notes'>
-                <p>*zipcode omitted when data not collected for that year</p>
+                <p>*zip code omitted from map when data is not recorded</p>
             </div>
 
             <div className="footer-citation">
-                <p className="footer-author">Graphic by Quinn Dizon</p>
+                <p className="footer-author">Graphic by Quinn Dizon | Sep. 15, 2019</p>
                 <p className="footer-source">Source: <a href="https://www.zillow.com/research/data/" target="_blank">Zillow economic data</a></p>
             </div>
         </footer>
--- src/components/TitleHeading.js
+++ src/components/TitleHeading.js
@@ -5,7 +5,7 @@ const TitleHeading = () => {
     return (
         <header className="title-heading">
             <h3>How have D.C. home prices changed since 2000?</h3>
-            <p>Median home value by year separated by zipcode*</p>
+            <p>Median home value by year separated by zip code*</p>
         </header>
     )
 
