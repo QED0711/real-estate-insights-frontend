80d620d4f6c58b4486d1bb721e2be846aa840d9f
QED0711 <qdizon@gmail.com>
2019-09-15 15:12:28 -0400

    map sizing

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,8 +3,8 @@ import React from 'react';
 const MapContainer = ({ currentMap }) => {
     return (
         <div className="map-container">
-            <iframe className="map-frame" src={`https://qed0711.github.io/real_estate_insights/map-${currentMap}.html`} height="500"
-                width="500" />
+            <iframe className="map-frame" src={`https://qed0711.github.io/real_estate_insights/map-${currentMap}.html`}
+            />
         </div >
     )
 }
